d8db03f91e01d44194b8a8b0d316e42badf24046
alwaystiys <365825701@qq.com>
2023-04-03 18:21:09 +0800

    add test

--- assets/scripts/PlayerController.ts
+++ assets/scripts/PlayerController.ts
@@ -1,14 +1,18 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, EventMouse, input, Input } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
     start() {
-
+        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
     }
 
     update(deltaTime: number) {
         
+    }
+
+    onMouseUp(event : EventMouse){
+
     }
 }
 
